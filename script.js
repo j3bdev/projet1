efae7ac9538f42ee9eaa88373f6f38610a736680
nawak44 <84684664+nawak44@users.noreply.github.com>
2022-09-29 21:15:34 +0200

    update JS + actualisation de chaque page

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 function openCloseNav() {
   let ul = document.querySelector("#list-menu");
-  if (ul.style.display === "none") {
-    ul.style.display = "flex";
-  } else {
+  if (ul.style.display != "none") {
     ul.style.display = "none";
+  } else {
+    ul.style.display = "flex";
   }
 }
